0d6b136174f7abf2a479526436d1c922da52fda0
djgalati <38479396+djgalati@users.noreply.github.com>
2018-07-12 18:41:57 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const url = require('url');
+ 
+function Arduino(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+ 
+arduino.prototype = {
+ 
+  getSwitchOnCharacteristic: function (next) {
+    const me = this;
+    request({
+        url: me.getUrl,
+        method: 'GET',
+    }, 
+    function (error, response, body) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next(null, body.currentState);
+    });
+  },
+   
+  setSwitchOnCharacteristic: function (on, next) {
+    const me = this;
+    request({
+      url: me.postUrl,
+      body: {'targetState': on},
+      method: 'POST',
+      headers: {'Content-type': 'application/json'}
+    },
+    function (error, response) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next();
+    });
+  }
+};
